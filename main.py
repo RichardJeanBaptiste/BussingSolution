d91797cd88877d8e2961766f1fb070aa171c8aee
Richard Jean-Baptiste <Richinbk1@gmail.com>
2025-07-20 07:40:25 -0400

    added start/stop sockets

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app.config['SECRET_KEY'] = 'your_secret'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 conn_count = 0
+is_busing = False
 
 @app.route('/')
 def index():
@@ -16,12 +17,32 @@ def handle_connect():
     global conn_count
     print('Client connected:')
     conn_count += 1
+
     emit('CResponse', {'data': 'Connected!', 'count': conn_count})
 
+    if is_busing:
+        emit('start')
+    
+    
+@socketio.on('start_signal')
+def start_event():
+    global is_busing
+    print("Start event signaled")
+    is_busing = True
+    emit('start', broadcast=True)
+
+@socketio.on('stop_signal')
+def stop_event():
+    global is_busing
+    print("Stop event signaled")
+    is_busing = False
+    emit('stop', broadcast=True)
+
 @socketio.on('disconnect')
-def handle_disconnect():
+def handle_disconnect(reason):
+    global conn_count
     conn_count -= 1
-    print('Client disconnected:')
+    print(f'Client disconnected: remaining connections : {conn_count}')
 
 @socketio.on_error()
 def error_handler(e):
--- static/index.js
+++ static/index.js
@@ -1,15 +1,33 @@
+let activeDiv = document.getElementById("active");
 console.log("Connected to html Pages")
 
 let socket = io();
+
 
 // Event handler for 'connect'
 socket.on('connect', function() {
     console.log('Connected to server!');
 });
 
+document.getElementById('test').addEventListener('click', () => {
+    socket.emit('start_signal');
+});
+
+document.getElementById('stop').addEventListener('click', () => {
+    socket.emit('stop_signal');
+})
+
+socket.on('start', () => {
+    activeDiv.innerHTML = 'Active';
+})
+
+socket.on('stop', () => {
+    activeDiv.innerHTML = "Not Active";
+})
+
 socket.on('CResponse', function(data){
     console.log(data)
-})
+});
 
 // Event handler for 'disconnect'
 socket.on('disconnect', function() {
